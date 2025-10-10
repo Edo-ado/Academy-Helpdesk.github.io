4e02ca9135fbbe9ce7fa5cd65da14e6a68500f2b
Edo-Ado-Uni <edulloamu@est.utn.ac.cr>
2025-10-10 17:18:13 -0600

    Carpeta de Api y React Con tailwind uwu Nya

---
+++ api/index.php
@@ -0,0 +1,38 @@
+<?php
+// Composer autoloader
+require_once 'vendor/autoload.php';
+/*Encabezada de las solicitudes*/
+/*CORS*/
+header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+header('Content-Type: application/json');
+
+/*--- Requerimientos Clases o librerías*/
+require_once "controllers/core/Config.php";
+require_once "controllers/core/HandleException.php";
+require_once "controllers/core/Logger.php";
+require_once "controllers/core/MySqlConnect.php";
+require_once "controllers/core/Request.php";
+require_once "controllers/core/Response.php";
+//Middleware
+require_once "middleware/AuthMiddleware.php";
+
+/***--- Agregar todos los modelos*/
+require_once "models/DirectorModel.php";
+require_once "models/ActorModel.php";
+require_once "models/GenreModel.php";
+require_once "models/MovieModel.php";
+require_once "models/ImageModel.php";
+
+/***--- Agregar todos los controladores*/
+require_once "controllers/MovieController.php";
+require_once "controllers/ActorController.php";
+
+//Enrutador
+require_once "routes/RoutesController.php";
+$index = new RoutesController();
+$index->index();
+
+
+
