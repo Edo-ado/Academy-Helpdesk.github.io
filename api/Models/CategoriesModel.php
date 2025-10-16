0357ede14a976f0f30c57cd70b3ca76589bd011d
Ashkituru <assibajaro@est.utn.ac.cr>
2025-10-16 12:39:29 -0600

    Refactor GetCategoryDetailsByID method: enhance SQL query to include tags and improve readability with aliases.

--- api/Models/CategoriesModel.php
+++ api/Models/CategoriesModel.php
@@ -23,8 +23,13 @@ public function GetCategoryById($id) {
 
 //nombre de la categoria, lista de etiquetas, lista de especialidades y su SLA
 public function GetCategoryDetailsByID($id) {
-    $vSql = "SELECT Categories.Name, SLA.MinTimeHours,SLA.MaxTimeHours, Specialities.Speciality FROM Categories LEFT JOIN SLA ON Categories.SLAId = SLA.Id
-LEFT JOIN Specialities ON Specialities.CategoryId = Categories.Id WHERE Categories.Id = $id;";
+    $vSql = "SELECT Categories.Name AS Categorie, SLA.MinTimeHours AS TiempoMinimo, SLA.MaxTimeHours AS TiempoMaximo, GROUP_CONCAT(DISTINCT Specialities.Speciality SEPARATOR ', ') AS Especielities, GROUP_CONCAT(DISTINCT Tags.Tag SEPARATOR ', ') AS tags           
+FROM Categories
+JOIN SLA ON Categories.SLAId = SLA.Id
+JOIN Specialities ON Specialities.CategoryId = Categories.Id
+JOIN Tags ON Tags.CategoryId = Categories.Id
+WHERE Categories.Id = $id
+";
     $vResultado = $this->enlace->ExecuteSQL($vSql, [$id]);
     return $vResultado;
 }
