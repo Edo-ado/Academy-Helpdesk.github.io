de45fbefbdd896076f670adce639864791727b3f
Edo-Ado-Uni <edulloamu@est.utn.ac.cr>
2025-11-17 17:33:42 -0600

    Sla

--- api/Models/SLAModel.php
+++ api/Models/SLAModel.php
@@ -1,32 +1,47 @@
 <?php
-class SLAModel  {
+class SLAModel
+{
 
-    public $enlace;
+   public $enlace;
 
-    public function __construct()
-    {
-        $this->enlace = new MySqlConnect();
-    }
-
-    
- public function GetAllSLAs() {
-    $vSql = "SELECT * FROM sla where Active=1;";
-    $vResultado = $this->enlace->ExecuteSQL($vSql);
-    return $vResultado;
- }
-
- public function GetSLABYId($id) {
-    $vSql = "SELECT * FROM sla WHERE id = $id;";
-    $vResultado = $this->enlace->ExecuteSQL($vSql, [$id]);
-    return $vResultado;
- }
+   public function __construct()
+   {
+      $this->enlace = new MySqlConnect();
+   }
 
 
- public function CreateSLA($MaxTimeResponse, $MaxTimeResolution) {
-    $vSql = "INSERT INTO sla ( MaxTimeResponse, MaxTimeResolution, Active) VALUES ( $MaxTimeResponse, $MaxTimeResolution, 1);";
-    $vResultado = $this->enlace->executeSQL_DML($vSql);
-    return $vResultado;
- }
+   public function GetAllSLAs()
+   {
+      $vSql = "SELECT * FROM sla where Active=1;";
+      $vResultado = $this->enlace->ExecuteSQL($vSql);
+      return $vResultado;
+   }
+
+   public function GetSLABYId($id)
+   {
+      $vSql = "SELECT * FROM sla WHERE id = $id;";
+      $vResultado = $this->enlace->ExecuteSQL($vSql, [$id]);
+      return $vResultado;
+   }
+
+
+   public function CreateSLA($objeto)
+   {
+      $sql = "INSERT INTO sla (MaxTimeResponse, MaxTimeResolution)
+            VALUES ($objeto->MaxTimeResponse, $objeto->MaxTimeResolution);";
+
+      $this->enlace->executeSQL_DML($sql, );
+
+      $iduser = $this->enlace->executeSQL_DML_last($sql);
+
+
+      return [
+         "success" => true,
+         "message" => "Técnico creado correctamente",
+         "Id" => $iduser
+      ];
+   }
+
 
 
 }
