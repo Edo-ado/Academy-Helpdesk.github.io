c4b9dcbee8debe1c145708eaad8a72a63ca867a8
Edo-ado <edu18088@gmail.com>
2025-11-30 18:28:36 -0600

    autotriagemodel

---
+++ api/Models/AutotriageModel.php
@@ -0,0 +1,21 @@
+<?php
+class AutotriageModel
+{
+
+    public $enlace;
+
+    public function __construct()
+    {
+        $this->enlace = new MySqlConnect();
+    }
+
+    public function getTicketsPendientes(){
+        
+    }
+    
+
+
+
+
+
+}
